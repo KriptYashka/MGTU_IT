f40408b6cfae76aa0f06a08ce6ad98396eccef01
KriptYashka <kriptyashka@gmail.com>
2022-05-20 23:47:26 +0300

    Запросы на сервер для всех сущностей

--- main/sitetools/backrequest.py
+++ main/sitetools/backrequest.py
@@ -3,45 +3,71 @@ import json
 
 url = 'http://localhost:5000/'
 
-#  Students
 
+class BackRequest:
+    def __init__(self, postfix: str):
+        self.postfix = postfix
+        self.total_url = url + self.postfix
 
-def get_students():
-    total_url = url + "student"
-    response = requests.get(total_url)
-    result = response.json()
-    if response.status_code == 200:
-        return result["value"]
-    return None
-
-
-def get_student_by_id(student_id: str):
-    total_url = url + "student/" + student_id
-    response = requests.get(total_url)
-    if response.status_code == 200:
+    def get_all(self):
+        response = requests.get(self.total_url)
         result = response.json()
-        return result["value"]
-    return None
+        if response.status_code == 200:
+            return result["value"]
+        return None
+
+    def get_by_id(self, obj_id: str):
+        self.total_url += f"/{obj_id}"
+        response = requests.get(self.total_url)
+        if response.status_code == 200:
+            result = response.json()
+            return result["value"]
+        return None
+
+    def create(self, data: dict):
+        response = requests.post(self.total_url, data=data)
+        return response.status_code
+
+    def edit(self, data: dict):
+        response = requests.put(self.total_url, data=data)
+        return response.status_code
+
+    def delete(self, obj_id: str):
+        self.total_url += f"/{obj_id}"
+        response = requests.put(self.total_url)
+        return response.status_code
 
 
-def create_student(student_data):
-    total_url = url + "student/"
-    response = requests.post(total_url, data=student_data)
-    return response.status_code
+class StudentRequest(BackRequest):
+    def __init__(self):
+        super().__init__("student")
 
 
-def edit_student(student_data):
-    total_url = url + "student/"
-    response = requests.put(total_url, data=student_data)
-    return response.status_code
+class MentorRequest(BackRequest):
+    def __init__(self):
+        super().__init__("mentor")
 
 
-def delete_student(student_id: str):
-    total_url = url + "student/" + student_id
-    response = requests.delete(total_url)
-    return response.status_code
+class UserRequest(BackRequest):
+    def __init__(self):
+        super().__init__("user")
+
+
+class EventRequest(BackRequest):
+    def __init__(self):
+        super().__init__("event")
+
+
+class ThemeRequest(BackRequest):
+    def __init__(self):
+        super().__init__("theme")
+
+
+class CategoryRequest(BackRequest):
+    def __init__(self):
+        super().__init__("category")
 
 
 if __name__ == '__main__':
-    res = get_student_by_id("90ecf692-f920-4779-8bee-44e155c6ef24")
+    res = StudentRequest().get_all()
     print(res)
